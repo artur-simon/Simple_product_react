d21862758963bd2a4fa23ae5da46ce6e9c946dfb
Artur <artur_simon@hotmail.com>
2019-12-16 05:06:49 -0300

    add responsitivity in navbar and newsletter form, email validation, submit functionality using hooks, edit README.md, talk about the development process

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import useForm from '../Hooks/useForm'
 
 const Form = () => {
+  
+  const login = () => {
+    console.log(JSON.stringify(values));
+  };
+
+  const { values, handleChange, handleSubmit } = useForm(login);
+
   return (
     <section id="newsletter">
       <h2>Compartilhe a novidade</h2>
@@ -8,16 +16,16 @@ const Form = () => {
         Quer que seus amigos também ganhem a lista personalizada deles? Preencha
         agora!
       </p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="inputContainer">
           <div className="inputBox">
             <label id="name">Nome do seu amigo:</label>
-            <input className="inputField" type="text" id="name" required />
+            <input className="inputField" type="text" id="name" onChange={handleChange} value={values.name} required />
           </div>
 
           <div className="inputBox">
             <label id="email">Email:</label>
-            <input className="inputField" type="text" id="email" required />
+            <input className="inputField" type="email" id="email" onChange={handleChange} value={values.email} required />
           </div>
         </div>
 
